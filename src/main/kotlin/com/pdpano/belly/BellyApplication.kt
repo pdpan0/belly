61582f355c92a5e3caeeeb19c8542ca09f8a9741
pdpan0 <lm.m4rtins@gmail.com>
2022-09-23 07:14:17 -0300

    FEAT: Testing inversion dependency without annotations

--- src/main/kotlin/com/pdpano/belly/BellyApplication.kt
+++ src/main/kotlin/com/pdpano/belly/BellyApplication.kt
@@ -2,10 +2,14 @@ package com.pdpano.belly
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.support.ClassPathXmlApplicationContext
 
 @SpringBootApplication
 class BellyApplication
 
 fun main(args: Array<String>) {
 	runApplication<BellyApplication>(*args)
+
+	val appContext = ClassPathXmlApplicationContext("classpath:context.xml")
+
 }
